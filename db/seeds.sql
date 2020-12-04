a07c27e42b2bc8dc0ae7814ae2216d77511b8a63
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-12-04 16:12:38 -0500

    fixed heirarchy. refactored code.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Mad Love");
+INSERT INTO burgers (burger_name) VALUES ("Deluxe Double Bacon Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Old Timer");
